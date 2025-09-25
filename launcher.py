92952376784e0b70646a793542d28e90a18bcf01
Titan Computer <39844065+TitanComputer@users.noreply.github.com>
2025-09-25 13:41:19 +0330

    update launcher

--- launcher.py
+++ launcher.py
@@ -1,10 +1,9 @@
 import os
 import sys
-from streamlit.web import cli as stcli
+import subprocess
 
 if __name__ == "__main__":
-    # مسیر main.py نسبت به همین فایل launcher
     main_path = os.path.join(os.path.dirname(__file__), "main.py")
 
-    sys.argv = ["streamlit", "run", main_path]
-    sys.exit(stcli.main())
+    # اجرای Streamlit به صورت subprocess
+    subprocess.run([sys.executable, "-m", "streamlit", "run", main_path])
