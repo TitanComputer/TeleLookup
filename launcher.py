7dae3fc82d24ae067e0553049660d42cfe0a722f
Titan Computer <39844065+TitanComputer@users.noreply.github.com>
2025-09-25 19:24:41 +0330

    update launcher

--- launcher.py
+++ launcher.py
@@ -3,12 +3,12 @@ import os
 from streamlit.web import cli as stcli
 
 if __name__ == "__main__":
-    os.environ["STREAMLIT_DEVELOPMENT_MODE"] = "false"
     main_path = os.path.join(os.path.dirname(__file__), "main.py")
     sys.argv = [
         "streamlit",
         "run",
         main_path,
+        "--global.developmentMode=false",
         "--server.port=8501",
         "--server.enableCORS=false",
         "--server.enableXsrfProtection=false",
