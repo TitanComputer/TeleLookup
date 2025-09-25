1670f99f447f8181d889d2bcdd662e41ffcfc6dc
Titan Computer <39844065+TitanComputer@users.noreply.github.com>
2025-09-25 08:46:56 +0330

    update launcher

--- launcher.py
+++ launcher.py
@@ -1,5 +1,6 @@
-import subprocess
 import sys
+from streamlit.web import cli as stcli
 
 if __name__ == "__main__":
-    subprocess.run([sys.executable, "-m", "streamlit", "run", "main.py"])
+    sys.argv = ["streamlit", "run", "main.py"]
+    sys.exit(stcli.main())
