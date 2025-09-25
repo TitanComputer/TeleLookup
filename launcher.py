f2f09fa2e11eba1359504dc26d2ec00d2ec1dcb6
Titan Computer <39844065+TitanComputer@users.noreply.github.com>
2025-09-25 17:21:33 +0330

    update launcher

--- launcher.py
+++ launcher.py
@@ -4,5 +4,12 @@ from streamlit.web import cli as stcli
 
 if __name__ == "__main__":
     main_path = os.path.join(os.path.dirname(__file__), "main.py")
-    sys.argv = ["streamlit", "run", main_path]
+    sys.argv = [
+        "streamlit",
+        "run",
+        main_path,
+        "--server.port=8501",
+        "--server.enableCORS=false",
+        "--server.enableXsrfProtection=false",
+    ]
     sys.exit(stcli.main())
