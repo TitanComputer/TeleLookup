61374df7926f7a514710b1e0e51c539d8d68c8dc
Titan Computer <39844065+TitanComputer@users.noreply.github.com>
2025-09-25 12:09:34 +0330

    update launcher

--- launcher.py
+++ launcher.py
@@ -1,6 +1,10 @@
+import os
 import sys
 from streamlit.web import cli as stcli
 
 if __name__ == "__main__":
-    sys.argv = ["streamlit", "run", "main.py"]
+    # مسیر main.py نسبت به همین فایل launcher
+    main_path = os.path.join(os.path.dirname(__file__), "main.py")
+
+    sys.argv = ["streamlit", "run", main_path]
     sys.exit(stcli.main())
