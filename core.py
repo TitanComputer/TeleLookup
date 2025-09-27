5773c14ff1ff25c6c9b7014b68dcbafd693b1a6b
Titan Computer <39844065+TitanComputer@users.noreply.github.com>
2025-09-27 14:02:57 +0330

    refactor process chunk to have better performance

--- core.py
+++ core.py
@@ -4,18 +4,31 @@ def count_lines_fast(filename):
         return sum(buf.count(b"\n") for buf in iter(lambda: f.read(1024 * 1024), b""))
 
 
+# ---------- process chunk ----------
 def process_chunk(chunk, parse_line, append, add, id_q, user_q, phone_q, seen_ids, results_list):
+    # آماده‌سازی queryها بیرون از حلقه
+    id_q = id_q or None
+    user_q = user_q.lower() if user_q else None
+    phone_q = phone_q or None
+
+    local_add = add
+    local_append = append
+    local_seen = seen_ids
+
     for line in chunk:
         parsed = parse_line(line)
         if not parsed:
             continue
-        if id_q and id_q not in parsed["id"]:
+
+        pid = parsed["id"]
+
+        if id_q and id_q not in pid:
             continue
         if user_q and user_q not in parsed["username"].lower():
             continue
         if phone_q and phone_q not in parsed["phone"]:
             continue
-        pid = parsed["id"]
-        if pid not in seen_ids:
-            add(pid)
-            append(parsed)
+
+        if pid not in local_seen:
+            local_add(pid)
+            local_append(parsed)
