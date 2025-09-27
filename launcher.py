a82d1dfe399f1eda2916b1bc53545f8705b32d55
Titan Computer <39844065+TitanComputer@users.noreply.github.com>
2025-09-27 22:35:35 +0330

    add close splash to launcher

--- launcher.py
+++ launcher.py
@@ -1,8 +1,32 @@
 import sys
 import os
+import time
+import tempfile
 from streamlit.web import cli as stcli
 
+
+def close_splash():
+    """Close Nuitka splash screen if running in onefile mode"""
+    if "NUITKA_ONEFILE_PARENT" in os.environ:
+        splash_filename = os.path.join(
+            tempfile.gettempdir(),
+            "onefile_%d_splash_feedback.tmp" % int(os.environ["NUITKA_ONEFILE_PARENT"]),
+        )
+        if os.path.exists(splash_filename):
+            try:
+                os.unlink(splash_filename)
+            except OSError:
+                pass
+
+
 if __name__ == "__main__":
+
+    # (اختیاری) کمی تأخیر بدی تا مطمئن بشی استریم‌لیت بالا میاد
+    time.sleep(5)
+
+    # بستن اسپلش
+    close_splash()
+
     main_path = os.path.join(os.path.dirname(__file__), "main.py")
     sys.argv = [
         "streamlit",
