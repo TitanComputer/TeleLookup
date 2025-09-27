1be843948cca92670835c2e770e16c0ad9c075a4
Titan Computer <39844065+TitanComputer@users.noreply.github.com>
2025-09-27 08:11:36 +0330

    update launcher for autopytoexe

--- launcher.py
+++ launcher.py
@@ -1,18 +1,17 @@
+import sys
 import os
-from streamlit.web import bootstrap
+from streamlit.web import cli as stcli
 
 if __name__ == "__main__":
-    # مسیر فایل اصلی برنامه
     main_path = os.path.join(os.path.dirname(__file__), "main.py")
-
-    # اینجا تنظیمات سرور رو مستقیماً پاس می‌دی
-    bootstrap.load_config_options(
-        flag_options={
-            "server.port": 8501,
-            "server.enableCORS": False,
-            "server.enableXsrfProtection": False,
-            "global.developmentMode": False,  # کلید اصلی
-        }
-    )
-
-    bootstrap.run(main_path, "", [], flag_options={})
+    sys.argv = [
+        "streamlit",
+        "run",
+        main_path,
+        "--global.developmentMode=false",
+        "--server.port=8501",
+        "--server.enableCORS=false",
+        "--server.enableXsrfProtection=false",
+        "--browser.gatherUsageStats=false",
+    ]
+    sys.exit(stcli.main())
