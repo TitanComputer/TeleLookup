9ca9017b651e9f7d1d09f103c44e3151f6cbd3da
Titan Computer <39844065+TitanComputer@users.noreply.github.com>
2025-09-29 19:08:43 +0330

    bump version

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from core import *
 
-APP_VERSION = "1.2.0"
+APP_VERSION = "1.3.0"
 
 # ===== ایجاد shared_state در سطح global/script
 if "shared_state" not in st.session_state:
